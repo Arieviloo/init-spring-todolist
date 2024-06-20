a1e9a9de67c8976902321de88f605752764fb35f
Jadiê Oliveira <jadieoliveira00@gmail.com>
2024-06-20 13:33:33 -0300

    Add controller

---
+++ src/main/java/com/example/todolist/controller/TodoController.java
@@ -0,0 +1,36 @@
+package com.example.todolist.controller;
+
+import java.util.List;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.todolist.entity.Todo;
+import com.example.todolist.service.TodoService;
+
+@RestController
+@RequestMapping("/todos")
+public class TodoController {
+    private TodoService todoService;
+
+    public TodoController(TodoService todoService) {
+        this.todoService = todoService;
+    }
+
+    List<Todo> list() {
+        return todoService.list();
+    }
+
+    List<Todo> create(Todo todo) {
+        return todoService.create(todo);
+    }
+
+    List<Todo> update(Todo todo) {
+        return todoService.update(todo);
+    }
+
+    List<Todo> delete(Long id) {
+        return todoService.delete(id);
+    }
+
+}
